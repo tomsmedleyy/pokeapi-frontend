5fefd7ad1effab6ecb7b7e9260323ebe32cf6b13
Tom Smedley <social.thomassmedley@gmail.com>
2022-03-03 17:11:10 +0000

    Add Types component

---
+++ src/components/Types.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+import { PokemonType } from "types/pokemon";
+
+interface TypesProps {
+  pokemon: PokemonType;
+}
+
+export const Types: React.FC<TypesProps> = ({ pokemon }) => {
+  return (
+    <div className="mt-2 flex gap-2 flex-no-wrap">
+      {pokemon.types.map((item) => (
+        <p
+          className={`capitalize px-4 pt-2 pb-1 rounded-full ${
+            pokemon.color === "white" ? "bg-black/10" : "text-white bg-white/10"
+          }`}
+        >
+          {item.type.name}
+        </p>
+      ))}
+    </div>
+  );
+};
