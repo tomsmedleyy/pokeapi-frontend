4ce143ccdc1c23efea26ac1d78ee51b7843305cd
Tom Smedley <social.thomassmedley@gmail.com>
2022-03-03 21:13:29 +0000

    Correctly map pokemon types

--- src/components/Types.tsx
+++ src/components/Types.tsx
@@ -9,8 +9,9 @@ interface TypesProps {
 export const Types: React.FC<TypesProps> = ({ pokemon }) => {
   return (
     <div className="mt-2 flex gap-2 flex-no-wrap">
-      {pokemon.types.map((item) => (
+      {pokemon.types.map((item, key) => (
         <p
+          key={key}
           className={`capitalize px-4 pt-2 pb-1 rounded-full ${
             pokemon.color === "white" ? "bg-black/10" : "text-white bg-white/10"
           }`}
