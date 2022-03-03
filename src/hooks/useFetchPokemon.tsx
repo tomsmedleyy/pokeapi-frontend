1298d3f5d8261a50fbcc806d171cad4d65fa0b7f
Tom Smedley <social.thomassmedley@gmail.com>
2022-03-03 14:03:25 +0000

    get pokemon hook

---
+++ src/hooks/useFetchPokemon.tsx
@@ -0,0 +1,43 @@
+import { api, useAPI } from "services/api";
+import useSWR from "swr";
+
+import { URLReference } from "types/common";
+import { PokemonType, PokemonList } from "types/pokemon";
+
+const pokemonFetcher = async (items: URLReference[]) => {
+  const requests = items.map(
+    async (item) =>
+      await api.get<PokemonType>(item.url).then(async ({ data }) => {
+        const pokemon = data;
+        await api.get(pokemon.species.url).then(({ data }) => {
+          pokemon.color = data.color.name;
+        });
+        return pokemon;
+      })
+  );
+  return Promise.all([...requests]).then((response) => response);
+};
+
+export const useFetchPokemon = () => {
+  const { data, error, isLoading, mutate } = useAPI<PokemonList>(
+    "/pokemon?limit=151"
+  );
+
+  const { data: pokemon, error: pokemonError } = useSWR(
+    data ? [data.results] : null,
+    pokemonFetcher,
+    {
+      shouldRetryOnError: false,
+      revalidateOnFocus: false,
+    }
+  );
+
+  const pokemonLoading = !pokemon && !pokemonError;
+
+  return {
+    data: pokemon,
+    error: error ?? pokemonError ?? null,
+    isLoading: pokemonLoading || isLoading,
+    mutate,
+  };
+};
