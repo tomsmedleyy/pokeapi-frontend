5cbbaa74659f96e36394443ba9a1deb93011b60e
Tom Smedley <social.thomassmedley@gmail.com>
2022-03-08 13:56:52 +0000

    Add Endless scrolling

--- src/features/PokemonList.tsx
+++ src/features/PokemonList.tsx
@@ -1,24 +1,40 @@
 import React from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
 
+import { usePokemonList } from "hooks/usePokemonList";
 import { useFetchPokemon } from "hooks/useFetchPokemon";
 
 import { Pokemon } from "components/Pokemon";
 import { Loader } from "components/Loader";
 
 export const PokemonList: React.FC = () => {
-  const { data, isLoading } = useFetchPokemon();
   const [searchTerm, setSearchTerm] = React.useState("");
+  const [nextResult, setNextResults] = React.useState<undefined | string>(
+    undefined
+  );
+
+  const { data, next, isLoading } = useFetchPokemon(nextResult);
+  const { list, updateList } = usePokemonList();
+
+  React.useEffect(() => {
+    updateList(data || []);
+  }, [data, updateList]);
 
   const items = React.useMemo(() => {
-    if (data) {
+    if (list) {
       if (searchTerm) {
-        return data.filter((item) => item.name.includes(searchTerm));
+        return list.filter((item) => item.name.includes(searchTerm));
       }
-      return data;
+      return list;
     }
-  }, [data, searchTerm]);
+  }, [list, searchTerm]);
 
-  if (isLoading || !data) return <Loader />;
+  const handleFetchMore = () => {
+    setNextResults(next || "");
+    console.log(next);
+  };
+
+  if ((isLoading || !data) && list.length === 0) return <Loader />;
   return (
     <div className="container pb-20">
       <div className="p-6 pt-14 pb-10 flex flex-col justify-between items-center md:flex-row">
@@ -39,11 +55,20 @@ export const PokemonList: React.FC = () => {
           Found {items?.length || "0"} items for "{searchTerm}"
         </p>
       ) : null}
-      <div className="grid gap-8 grid-cols-1 mx-6 md:mx-auto md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-        {items
-          ? items.map((pokemon, key) => <Pokemon key={key} pokemon={pokemon} />)
-          : null}
-      </div>
+      <InfiniteScroll
+        dataLength={items?.length || 0}
+        next={handleFetchMore}
+        hasMore={true}
+        loader={<Loader inline />}
+      >
+        <div className="grid gap-8 grid-cols-1 mx-6 pt-8 md:mx-auto md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+          {items
+            ? items.map((pokemon, key) => (
+                <Pokemon key={key} pokemon={pokemon} />
+              ))
+            : null}
+        </div>
+      </InfiniteScroll>
     </div>
   );
 };
