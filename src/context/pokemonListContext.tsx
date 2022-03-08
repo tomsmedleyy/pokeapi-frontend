b842c8c38b8cc314c536d30c72ab41203b765df3
Tom Smedley <social.thomassmedley@gmail.com>
2022-03-08 15:12:40 +0000

    Make sure data is unique

--- src/context/pokemonListContext.tsx
+++ src/context/pokemonListContext.tsx
@@ -15,9 +15,19 @@ export const PokemonListContext = React.createContext<IPokemonListContext>({
 export const PokemonListProvider: React.FC = ({ children }) => {
   const [list, setList] = React.useState<PokemonType[]>([]);
 
+  const unique = (array: any) => {
+    const arr = array.concat();
+    for (let i = 0; i < arr.length; ++i) {
+      for (let j = i + 1; j < arr.length; ++j) {
+        if (arr[i] === arr[j]) arr.splice(j--, 1);
+      }
+    }
+    return arr;
+  };
+
   const updateList = React.useCallback(
     (items: PokemonType[]) => {
-      setList((currentList) => [...currentList, ...items]);
+      setList((currentList) => unique([...currentList, ...items]));
     },
     [setList]
   );
